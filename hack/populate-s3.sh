cbd05b49589b3fc5a72ed057beed1da3a0197a4a
Davanum Srinivas <davanum@gmail.com>
2023-07-03 11:31:40 -0400

    Build amazon-eks-ami based AMI from scratch for testing
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

---
+++ hack/populate-s3.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+mkdir -p "${ROOT}/_output"
+
+if [[ -z "$(which aarch64-linux-gnu-gcc)" || -z "$(which x86_64-linux-gnu-gcc)" ]]; then
+  echo "Can't find aarch64-linux-gnu-gcc x86_64-linux-gnu-gcc or in PATH, please install and retry"
+  if [[ -n "$(command -v lsb_release)" && $(lsb_release -si) == "Ubuntu" ]]; then
+    echo "using : apt install -y gcc-aarch64-linux-gnu"
+    [[ "$(id -u)" == "0" ]] && apt update && apt install -y gcc-aarch64-linux-gnu
+  elif [[ -n "$(command -v lsb_release)" && $(lsb_release -si) == "Debian" ]]; then
+    echo "using : apt install -y gcc-aarch64-linux-gnu"
+    [[ "$(id -u)" == "0" ]] && apt update && apt install -y gcc-aarch64-linux-gnu
+  elif [[ $OSTYPE == 'darwin'* ]]; then
+    echo "using : brew tap messense/macos-cross-toolchains && brew install x86_64-unknown-linux-gnu aarch64-unknown-linux-gnu"
+  fi
+fi
+
+DATE=$(date -u +'%Y-%m-%d')
+
+# Generate kubernetes binaries
+pushd "$(go env GOPATH)/src/k8s.io/kubernetes" >/dev/null
+
+  VERSION=$(hack/print-workspace-status.sh | grep gitVersion | awk '{print $2}' | sed -E 's/v([0-9]+)\.([0-9]+)\.([0-9]+).*/v\1.\2.\3/')
+  BIN_DIR="${ROOT}/_output/${VERSION}/${DATE}/bin"
+
+  make kubectl KUBE_BUILD_PLATFORMS="darwin/amd64" && \
+    make kubectl KUBE_BUILD_PLATFORMS="darwin/arm64" && \
+    make kubectl kubelet kube-proxy KUBE_BUILD_PLATFORMS="linux/arm64" && \
+    make kubectl kubelet kube-proxy KUBE_BUILD_PLATFORMS="linux/amd64" && \
+    make kubectl kubelet kube-proxy KUBE_BUILD_PLATFORMS="windows/amd64"
+
+  mkdir -p "${BIN_DIR}/darwin/amd64"
+  cp "_output/local/bin/darwin/amd64/kubectl" "${BIN_DIR}/darwin/amd64"
+
+  mkdir -p "${BIN_DIR}/darwin/arm64"
+  cp "_output/local/bin/darwin/arm64/kubectl" "${BIN_DIR}/darwin/arm64"
+
+  mkdir -p "${BIN_DIR}/linux/amd64"
+  cp "_output/local/bin/linux/amd64/kubectl" "${BIN_DIR}/linux/amd64"
+  cp "_output/local/bin/linux/amd64/kube-proxy" "${BIN_DIR}/linux/amd64"
+  cp "_output/local/bin/linux/amd64/kubelet" "${BIN_DIR}/linux/amd64"
+
+  mkdir -p "${BIN_DIR}/linux/arm64"
+  cp "_output/local/bin/linux/arm64/kubectl" "${BIN_DIR}/linux/arm64"
+  cp "_output/local/bin/linux/arm64/kube-proxy" "${BIN_DIR}/linux/arm64"
+  cp "_output/local/bin/linux/arm64/kubelet" "${BIN_DIR}/linux/arm64"
+
+  mkdir -p "${BIN_DIR}/windows/amd64"
+  cp "_output/local/bin/windows/amd64/kubectl.exe" "${BIN_DIR}/windows/amd64"
+  cp "_output/local/bin/windows/amd64/kube-proxy.exe" "${BIN_DIR}/windows/amd64"
+  cp "_output/local/bin/windows/amd64/kubelet.exe" "${BIN_DIR}/windows/amd64"
+
+popd
+
+[[ ! -d "$(go env GOPATH)/src/kubernetes-sigs/aws-iam-authenticator" ]] && \
+  git clone https://github.com/kubernetes-sigs/aws-iam-authenticator "$(go env GOPATH)/src/sigs.k8s.io/aws-iam-authenticator"
+
+# Generate aws-iam-authenticator binaries
+pushd "$(go env GOPATH)/src/sigs.k8s.io/aws-iam-authenticator" >/dev/null
+  VERSION=$(git describe --tags `git rev-list --tags --max-count=1`)
+  echo ${VERSION/#v} > version.txt
+  make build-all-bins
+
+  cp "_output/bin/aws-iam-authenticator_${VERSION}_darwin_amd64" "${BIN_DIR}/darwin/amd64/aws-iam-authenticator"
+  #cp "${ROOT}/_output/local/bin/darwin/arm64/aws-iam-authenticator" "${BIN_DIR}/darwin/arm64/aws-iam-authenticator"
+  cp "_output/bin/aws-iam-authenticator_${VERSION}_linux_amd64" "${BIN_DIR}/linux/amd64/aws-iam-authenticator"
+  cp "_output/bin/aws-iam-authenticator_${VERSION}_linux_arm64" "${BIN_DIR}/linux/arm64/aws-iam-authenticator"
+  cp "_output/bin/aws-iam-authenticator_${VERSION}_windows_amd64.exe" "${BIN_DIR}/windows/amd64/aws-iam-authenticator.exe"
+
+popd
+
+[[ ! -d "$(go env GOPATH)/src/k8s.io/cloud-provider-aws" ]] && \
+  git clone https://github.com/kubernetes/cloud-provider-aws "$(go env GOPATH)/src/k8s.io/cloud-provider-aws"
+
+# Generate cloud-provider-aws binaries
+pushd "$(go env GOPATH)/src/k8s.io/cloud-provider-aws" >/dev/null
+  make crossbuild-ecr-credential-provider
+
+  cp "ecr-credential-provider-linux-amd64" "${BIN_DIR}/linux/amd64/ecr-credential-provider"
+  cp "ecr-credential-provider-linux-arm64" "${BIN_DIR}/linux/arm64/ecr-credential-provider"
+  cp "ecr-credential-provider-windows-amd64" "${BIN_DIR}/windows/amd64/ecr-credential-provider"
+popd
+
+curl -sL https://github.com/containernetworking/cni/releases/download/v0.6.0/cni-amd64-v0.6.0.tgz -o "${BIN_DIR}/linux/amd64/cni-amd64-v0.6.0.tgz"
+curl -sL https://github.com/containernetworking/cni/releases/download/v0.6.0/cni-arm64-v0.6.0.tgz -o "${BIN_DIR}/linux/arm64/cni-arm64-v0.6.0.tgz"
+
+curl -sL https://github.com/containernetworking/plugins/releases/download/v0.8.6/cni-plugins-linux-amd64-v0.8.6.tgz -o "${BIN_DIR}/linux/amd64/cni-plugins-linux-amd64-v0.8.6.tgz"
+curl -sL https://github.com/containernetworking/plugins/releases/download/v0.8.6/cni-plugins-linux-amd64-v0.8.6.tgz -o "${BIN_DIR}/linux/arm64/cni-plugins-linux-arm64-v0.8.6.tgz"
+
+pushd _output >/dev/null
+  find . -name "*.sha256*" -delete
+  find . -name "*.sha1*" -delete
+  find . -name "*.md5*" -delete
+  for f in $(find . -type f | sort); do
+      dirname=$(dirname $f)
+      filename=$(basename $f)
+      pushd $dirname >/dev/null
+        shasum -a 1 "$filename" > "$filename".sha1;
+        shasum -a 256 "$filename" > "$filename".sha256;
+        md5sum "$filename" > "$filename".md5;
+      popd
+  done
+popd
+
+S3_BUCKET=${S3_BUCKET:-"provider-aws-test-infra"}
+aws s3 sync _output/ "s3://${S3_BUCKET}/"
