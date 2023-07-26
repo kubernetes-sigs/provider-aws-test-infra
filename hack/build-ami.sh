7b79314d65b68f21ecae585491e52790b9f3180c
Davanum Srinivas <davanum@gmail.com>
2023-07-26 07:24:08 -0400

    fix typo in file name
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- hack/build-ami.sh
+++ hack/build-ami.sh
@@ -42,7 +42,7 @@ pushd "$(go env GOPATH)/src/github.com/awslabs/amazon-eks-ami" >/dev/null
     export PACKER_DEFAULT_VARIABLE_FILE=eks-worker-al2023-variables.json
     export PACKER_TEMPLATE_FILE=eks-worker-al2023.json
   else
-    export PACKER_DEFAULT_VARIABLE_FILE=eks-worker-al-variables.json
+    export PACKER_DEFAULT_VARIABLE_FILE=eks-worker-al2-variables.json
     export PACKER_TEMPLATE_FILE=eks-worker-al2.json
   fi
   if [[ ${BUILD_EKS_AMI_ARCH:-""} == "arm64" ]]; then
