a50768498eb86d39d1f6a21e5a7233101fa7992b
Davanum Srinivas <davanum@gmail.com>
2023-08-10 14:50:08 -0400

    tweak k8s git repo directory
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- kubetest2-ec2/pkg/deployer/build.go
+++ kubetest2-ec2/pkg/deployer/build.go
@@ -42,6 +42,7 @@ func (d *deployer) Build() error {
 		u.Concurrency = 10
 	})
 	d.BuildOptions.CommonBuildOptions.S3Uploader = s3Uploader
+	d.BuildOptions.CommonBuildOptions.RepoRoot = d.RepoRoot
 
 	err = d.BuildOptions.Validate()
 	if err != nil {
