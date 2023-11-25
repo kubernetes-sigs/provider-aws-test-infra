89d70a64bf8ab0cb932a8c3609ba059cce553c4e
Davanum Srinivas <davanum@gmail.com>
2023-11-25 18:06:17 -0500

    Do not delete amazon-ec2-net-utils in AMI itself
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- hack/build-ami.sh
+++ hack/build-ami.sh
@@ -38,6 +38,7 @@ pushd "$(go env GOPATH)/src/github.com/awslabs/amazon-eks-ami" >/dev/null
   # disable sha256 check
   sed -i 's/sudo sha256sum.*$//' scripts/install-worker.sh || true
   sed -i 's/.*99-default.link.*$//' scripts/install-worker.sh || true
+  sed -i 's/.*amazon-ec2-net-utils.*$//' scripts/install-worker.sh || true
   sed -i 's/amazon-eks/provider-aws-test-infra/' eks-worker-al2-variables.json
   sed -i "s/us-west-2/${AWS_REGION:-'us-east-1'}/" eks-worker-al2-variables.json
   if [[ ${BUILD_EKS_AMI_OS:-""} == "al2023" ]]; then
