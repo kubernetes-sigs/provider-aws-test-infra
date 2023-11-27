54a397dd01b4141e133304721ec1ff7816cf6388
Davanum Srinivas <davanum@gmail.com>
2023-11-27 10:10:38 -0500

    tune some network params for al2023
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- kubetest2-ec2/config/al2023.sh
+++ kubetest2-ec2/config/al2023.sh
@@ -11,6 +11,53 @@ else
   ARCH=amd64
 fi
 
+cat << EOF > /etc/sysctl.d/k8s.conf
+# Kubernetes Settings
+# from https://github.com/bottlerocket-os/bottlerocket/blob/develop/packages/release/release-sysctl.conf
+# from https://github.com/kubernetes/kops/blob/master/nodeup/pkg/model/sysctls.go
+
+# Increase the number of connections
+net.core.somaxconn = 32768
+
+# Maximum Socket Receive Buffer
+net.core.rmem_max = 16777216
+# Maximum Socket Send Buffer"
+net.core.wmem_max = 16777216
+
+# Increase the maximum total buffer-space allocatable
+net.ipv4.tcp_wmem = 4096 87380 16777216
+net.ipv4.tcp_rmem = 4096 87380 16777216
+
+# Increase the number of outstanding syn requests allowed
+net.ipv4.tcp_max_syn_backlog = 8096
+
+# For persistent HTTP connections
+net.ipv4.tcp_slow_start_after_idle = 0
+
+# Allow to reuse TIME_WAIT sockets for new connections
+# when it is safe from protocol viewpoint
+net.ipv4.tcp_tw_reuse = 1
+
+# Max number of packets that can be queued on interface input
+# If kernel is receiving packets faster than can be processed
+# this queue increases
+net.core.netdev_max_backlog = 16384
+
+# Enable IPv4 forwarding for container networking.
+net.ipv4.conf.all.forwarding = 1
+
+# Enable IPv6 forwarding for container networking.
+net.ipv6.conf.all.forwarding = 1
+
+# Bumped the default TTL to 255 (maximum)
+net.ipv4.ip_default_ttl = 255
+
+# Connection tracking to prevent dropped connections
+net.netfilter.nf_conntrack_max = 1048576
+net.netfilter.nf_conntrack_generic_timeout = 600
+EOF
+sysctl --system
+
 # Fix issues with no networking from pods
 sed -i "s/^MACAddressPolicy=.*/MACAddressPolicy=none/" /usr/lib/systemd/network/99-default.link
 systemctl restart systemd-resolved
