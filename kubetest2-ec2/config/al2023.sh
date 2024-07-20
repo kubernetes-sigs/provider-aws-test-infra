037654b1c03ea8246651a961fef992499143c1d7
Davanum Srinivas <davanum@gmail.com>
2024-07-20 09:14:58 -0400

    enable read permissions to grab container logs
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- kubetest2-ec2/config/al2023.sh
+++ kubetest2-ec2/config/al2023.sh
@@ -97,6 +97,10 @@ fi
 mkdir -p /etc/kubernetes/
 mkdir -p /etc/kubernetes/manifests
 mkdir -p /etc/eks/image-credential-provider/
+mkdir -p /var/log/pods/
+chmod -R g+s /var/log/pods/
+mkdir -p /var/log/containers/
+chmod -R g+s /var/log/containers/
 
 cat << EOF > /etc/kubernetes/kubeadm-join.yaml
 apiVersion: kubeadm.k8s.io/v1beta3
